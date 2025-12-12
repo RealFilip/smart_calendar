86b289ea70e8c22b670c80c292a0f08c7afe0fdf
Filip <filko2006@seznam.cz>
2025-12-12 09:48:04 +0100

    Initial commit

---
+++ functions.js
@@ -0,0 +1,408 @@
+//all DOM elements from index.html
+const addEventBtn = document.getElementById("add-event");
+const eventDesc = document.querySelectorAll(".event-desc");
+const createEventBtn = document.getElementById("create-event");
+const groupInput = document.getElementById("group");
+const descInput = document.getElementById("desc");
+const chooseDay = document.getElementById("choose-day");
+const chooseFrom = document.getElementById("choose-from");
+const chooseTo = document.getElementById("choose-to");
+const errorEventCreation = document.getElementById("error-event-creation");
+const grid = document.querySelector('.grid');
+const cDaysDisplay = document.querySelectorAll('.day');
+const colorChoice = document.getElementById("color-choice");
+const colorPicker = document.getElementById("color-picker");
+const colorOkButton = document.getElementById("color-ok-button");
+
+// create calendar variables
+const cdi = new Date();  // cdi = current date information
+const cYear = cdi.getFullYear();
+const cMonthNum = cdi.getMonth() + 1;
+const cDate = cdi.getDate();
+const firstDayDatabase = {
+    2025: 2,
+    2026: 3,
+    2027: 4,
+    2028: 5,
+    2029: 0,
+    2030: 1,
+    2031: 2,
+    2032: 3,
+    2033: 5,
+    2034: 6,
+    2035: 0,
+    2036: 2,
+    2037: 3,
+    2038: 4,
+    2039: 5,
+    2040: 0,
+    2041: 1,
+    2042: 2,
+    2043: 3,
+    2044: 5,
+    2045: 6,
+    2046: 0,
+    2047: 1,
+    2048: 3,
+    2049: 4,
+    2050: 5,
+}
+
+// checks for leap year
+function checkLeapYear(checkLYear) {
+    return (checkLYear % 4) === 0;
+}
+
+//name the month
+function getMonthName(monthNum) {
+    if (monthNum === 1) {
+        return "January";
+    } else if (monthNum === 2) {
+        return "February";
+    } else if (monthNum === 3) {
+        return "March";
+    } else if (monthNum === 4) {
+        return "April";
+    } else if (monthNum === 5) {
+        return "May";
+    } else if (monthNum === 6) {
+        return "June";
+    } else if (monthNum === 7) {
+        return "July";
+    } else if (monthNum === 8) {
+        return "August";
+    } else if (monthNum === 9) {
+        return "September";
+    } else if (monthNum === 10) {
+        return "October";
+    } else if (monthNum === 11) {
+        return "November";
+    } else if (monthNum === 12) {
+        return "December";
+    }
+}
+
+// prep code for the data needed in multiple functions
+let totalDays = 0;
+let cMonthDays = 0;
+let previousMonth = ['', 0, 0];
+let nextMonth = ['', 0, 0];
+
+// useful funciton calculate totalDays, previousMonth (name and days) and nextMonth (name and days)
+function calData(givenDate, givenMonth, givenYear) {
+    totalDays = 0;
+    cMonthDays = 0;
+    previousMonth = ['', 0, 0];
+    nextMonth = ['', 0, 0];
+    leapYear = checkLeapYear(givenYear);
+
+    let givenMonthAdapt = givenMonth - 1;
+    for (let i = 0; i <= givenMonthAdapt; i++) { //goes through (i + 1) amount of months and adds up total amount of days
+        if (i === 0) {
+            if (i < givenMonthAdapt) {
+                totalDays += 31;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 31;
+            previousMonth = ['December', 31, 12];
+            if (leapYear) {
+                nextMonth = ['February', 29, 2];
+            } else {
+                nextMonth = ['February', 28, 2];
+            }
+        } else if (i === 1) {
+            nextMonth = ['March', 31, 3];
+            previousMonth = ['January', 31, 1];
+            // checks if it's a leap year
+            if (leapYear) {
+                if (i < givenMonthAdapt) {
+                    totalDays += 29;
+                } else {
+                    totalDays += givenDate;
+                }
+                cMonthDays = 29;
+            } else {
+                if (i < givenMonthAdapt) {
+                    totalDays += 28;
+                } else {
+                    totalDays += givenDate;
+                }
+                cMonthDays = 28;
+            }
+        } else if (i === 2) {
+            if (i < givenMonthAdapt) {
+                totalDays += 31;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 31;
+            nextMonth = ['April', 30, 4];
+            // checks if it's a leap year
+            if (leapYear) {
+                previousMonth = ['February', 29, 2];
+            } else {
+                previousMonth = ['February', 28, 2];
+            }
+        } else if (i === 3) {
+            if (i < givenMonthAdapt) {
+                totalDays += 30;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 30;
+            previousMonth = ['March', 31, 3];
+            nextMonth = ['May', 31, 5];
+        } else if (i === 4) {
+            if (i < givenMonthAdapt) {
+                totalDays += 31;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 31;
+            previousMonth = ['April', 30, 4];
+            nextMonth = ['June', 30, 6];
+        } else if (i === 5) {
+            if (i < givenMonthAdapt) {
+                totalDays += 30;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 30;
+            previousMonth = ['May', 31, 5];
+            nextMonth = ['July', 31, 7];
+        } else if (i === 6) {
+            if (i < givenMonthAdapt) {
+                totalDays += 31;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 31;
+            previousMonth = ['June', 30, 6];
+            nextMonth = ['August', 31, 8]
+        } else if (i === 7) {
+            if (i < givenMonthAdapt) {
+                totalDays += 31;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 31;
+            previousMonth = ['July', 31, 7];
+            nextMonth = ['September', 30, 9];
+        } else if (i === 8) {
+            if (i < givenMonthAdapt) {
+                totalDays += 30;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 30;
+            previousMonth = ['August', 31, 8];
+            nextMonth = ['October', 31, 10];
+        } else if (i === 9) {
+            if (i < givenMonthAdapt) {
+                totalDays += 31;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 31;
+            previousMonth = ['September', 30, 9];
+            nextMonth = ['November', 30, 11];
+        } else if (i === 10) {
+            if (i < givenMonthAdapt) {
+                totalDays += 30;
+            } else {
+                totalDays += givenDate;
+            }
+            cMonthDays = 30;
+            previousMonth = ['October', 31, 10];
+            nextMonth = ['December', 31, 12];
+        } else if (i === 11) {
+            if (i < givenMonthAdapt) {
+                totalDays += 31;
+            } else {
+                totalDays += givenDate;
+            }
+            previousMonth = ['November', 30, 11];
+            nextMonth = ['January', 31, 1];
+            cMonthDays = 31;
+        }
+    }
+}
+
+// function finds the week of the given date info and displays it in the week title on the webpage and also returns the entire week dates in an array
+function displayWeek(given_Date, given_Month, given_Year) {
+    // CODE THAT DISPLAYS THE WEEK TITLE
+    // gets multiple needed values; cMonthDays, previousMonth, nextMonth, totalDays 
+    calData(given_Date, given_Month, given_Year);
+    let givenMonthName = getMonthName(given_Month);
+
+    // calculates current day of the week as a number, 0 is monday, 6 is sunday
+    let cDayInWeekNum = (totalDays + firstDayDatabase[given_Year]) % 7;
+    if (cDayInWeekNum === 0) { // it's a sunday
+        cDayInWeekNum = 6;
+    } else { 
+        cDayInWeekNum -= 1; 
+    }
+
+    let cWeek = Math.floor(((totalDays + firstDayDatabase[given_Year]) / 7) + 1);
+
+    // code to calculate this weeks monday and this weeks sunday, and display the dates after
+    let cWeekMondayDate;
+    let cWeekSundayDate;
+
+    //calculate monday and sunday dates, and display them in the week title
+    if (given_Date >= cDayInWeekNum && (given_Date + 6 - cDayInWeekNum) <= cMonthDays) {
+        cWeekMondayDate = given_Date - cDayInWeekNum;
+        cWeekSundayDate = cWeekMondayDate + 6;
+        weekDisplay.textContent = `Week ${cWeek}, ${cWeekMondayDate} - ${cWeekSundayDate} ${givenMonthName}, ${given_Year}`;
+    } else if (given_Date <= cDayInWeekNum) {
+        cWeekMondayDate = previousMonth[1] + given_Date - cDayInWeekNum;
+        cWeekSundayDate = given_Date + 6 - cDayInWeekNum;
+        weekDisplay.textContent = `Week ${cWeek}, ${cWeekMondayDate} ${previousMonth[0]}- ${cWeekSundayDate} ${givenMonthName}, ${given_Year}`;
+    } else if (cMonthDays > (given_Date + 6 - cDayInWeekNum)) {
+        cWeekMondayDate = given_Date - cDayInWeekNum;
+        cWeekSundayDate = 6 - cMonthDays + cWeekMondayDate;
+        weekDisplay.textContent = `Week ${cWeek}, ${cWeekMondayDate} - ${cWeekSundayDate} ${givenMonthName}, ${given_Year}`;
+    }
+
+    // CODE THAT DISPLAYS THE SEPARATE WEEK DATES
+    let dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    findWeekDates(cDate, cMonthNum, cYear);  // !! Important to assign cDate , cMonth, cYear for every switch in calendar placement
+    cDaysDisplay.forEach((el, i) => el.textContent = `${dayNames[i]}. ${cWeekDates[i]}`);
+}
+
+//creation of info data about the event (to then save in the calendar database)
+function findDate() {
+    // calculates current day of the week as a number, 0 is monday, 6 is sunday
+    let cDayInWeekNum = (totalDays + firstDayDatabase[cYear]) % 7;
+    if (cDayInWeekNum === 0) { // it's a sunday
+        cDayInWeekNum = 6;
+    } else { 
+        cDayInWeekNum -= 1; 
+    }
+
+    let diff = chooseDay.value - cDayInWeekNum;  //chooseDay is the input on the site (from main: script.js)
+    let foundDate;
+    let foundMonth;
+    let foundYear = cYear;
+    if (cDate + diff <= 0) {
+        foundDate = previousMonth[1] + cDate + diff;
+        foundMonth = previousMonth[2];
+        // checks if the previous month was december
+        if (previousMonth[2] === 12) {
+            foundYear -= 1;
+        }
+    } else if (cDate + diff > cMonthDays) {
+        foundDate = (cDate + diff) - cMonthDays;
+        foundMonth = nextMonth[2];
+        // checks if the next month is january
+        if (nextMonth[2] === 1) {
+            foundYear += 1;
+        }
+    } else {
+        foundDate = cDate + diff;
+        foundMonth = cMonthNum;
+    }
+
+    return `${foundDate}/${foundMonth}/${foundYear}`;
+}
+
+let cWeekDates = [];  // array for saving the dates of the week
+// function to find the dates for every day of the week of the given date
+function findWeekDates(given__Date, given__Month, given__Year) {
+    cWeekDates = [];
+    // gets multiple needed values; cMonthDays, previousMonth, nextMonth, totalDays 
+    calData(given__Date, given__Month, given__Year);
+
+    // calculates current day of the week as a number, 0 is monday, 6 is sunday
+    let cDayInWeekNum = (totalDays + firstDayDatabase[given__Year]) % 7;
+    if (cDayInWeekNum === 0) { // it's a sunday
+        cDayInWeekNum = 6;
+    } else { 
+        cDayInWeekNum -= 1; 
+    }
+
+    // code to calculate this weeks monday and this weeks sunday, and display the dates after
+    let cWeekMondayDate;
+
+    //calculate monday and sunday dates, and display them in the week title
+    if (given__Date >= cDayInWeekNum && (given__Date + 6 - cDayInWeekNum) <= cMonthDays) {
+        cWeekMondayDate = given__Date - cDayInWeekNum;
+    } else if (given__Date <= cDayInWeekNum) {
+        cWeekMondayDate = previousMonth[1] + given__Date - cDayInWeekNum;
+    } else if (cMonthDays > (given__Date + 6 - cDayInWeekNum)) {
+        cWeekMondayDate = given__Date - cDayInWeekNum;
+    }
+
+    cWeekDates.push(cWeekMondayDate);
+    let offsetNM = 0;  // offset value needed if a date or multiple dates of the week are in the next month
+    for (let i = 1; i < 7; i++) {
+        if (cWeekMondayDate + i <= cMonthDays) {
+            cWeekDates.push(cWeekMondayDate + i);
+        } else {
+            offsetNM += 1;
+            cWeekDates.push(offsetNM);
+        }
+    }
+
+    return (cWeekDates);
+}
+
+//function to check if two dates are in the same week
+function sameWeek(date1, date2) {
+    // gets the week (s) of the first and second date through two functions: extractFromDate to convert the '23/10/2006' to each separate integer needed, then displayWeek to get the weekdate arrays
+    let compWeek1 = findWeekDates(extractFromDate("date", date1), extractFromDate("month", date1), extractFromDate("year", date1));
+    let compWeek2 = findWeekDates(extractFromDate("date", date2), extractFromDate("month", date2), extractFromDate("year", date2));
+
+    // returns the result 
+    let comparison = true;
+    for (let i = 0; i < 7; i++) {
+        if (compWeek1[i] !== compWeek2[i]) {
+            comparison = false;
+        }
+    }
+    return comparison;
+}
+
+// function to extract the number from a date (date, month, year)
+function extractFromDate(extractionType, extDate) {
+    let extraction = false;
+    let extractedDate = "";
+    let extractedMonth = "";
+    let extractedYear = "";
+
+    // extract date
+    let i = 0;
+    while (!extraction) {
+        if (extDate[i] === "/") {
+            extraction = true;
+        } else {
+            extractedDate += extDate[i];
+        }
+        i++;
+    }
+
+    while (extraction) {
+        if (extDate[i] === "/") {
+            extraction = false;
+        } else {
+            extractedMonth += extDate[i];
+        }
+        i++;
+    } 
+
+    while (extDate[i]) {
+        extractedYear += extDate[i];
+        i++;
+    }
+
+    if (extractionType === "date") {
+        return parseInt(extractedDate);
+    } else if (extractionType === "month") {
+        return parseInt(extractedMonth);
+    } else if (extractionType === "year") {
+        return parseInt(extractedYear);
+    }
+}
+
+export { checkLeapYear, getMonthName, calData, displayWeek, findDate, findWeekDates, sameWeek, extractFromDate };
---
+++ script.js
@@ -0,0 +1,216 @@
+
+// imports multiple functions 
+import { checkLeapYear, getMonthName, calData, displayWeek, findDate, findWeekDates, sameWeek, extractFromDate } from './functions.js';
+
+//EVERYTHING CALENDAR RELATED
+// get all calendar document objects
+const weekDisplay = document.getElementById("week-display");
+const dayDisplay = document.querySelectorAll(".day");
+
+// create calendar variables
+const cdi = new Date();  // cdi = current date information
+const cYear = cdi.getFullYear();
+const cMonthNum = cdi.getMonth() + 1;
+const cDate = cdi.getDate();
+const firstDayDatabase = {
+    2025: 2,
+    2026: 3,
+    2027: 4,
+    2028: 5,
+    2029: 0,
+    2030: 1
+}
+
+//calculate the current week
+let totalDays = 0;
+let cMonthDays = 0;
+let previousMonth = ['', 0, 0];
+let nextMonth = ['', 0, 0];
+
+// add event to calendar
+const addEventBtn = document.getElementById("add-event");
+const eventDesc = document.querySelectorAll(".event-desc");
+const createEventBtn = document.getElementById("create-event");
+const groupInput = document.getElementById("group");
+const descInput = document.getElementById("desc");
+const chooseDay = document.getElementById("choose-day");
+const chooseFrom = document.getElementById("choose-from");
+const chooseTo = document.getElementById("choose-to");
+const errorEventCreation = document.getElementById("error-event-creation");
+const grid = document.querySelector('.grid');
+const colorChoice = document.getElementById("color-choice");
+const colorPicker = document.getElementById("color-picker");
+const colorOkButton = document.getElementById("color-ok-button");
+
+addEventBtn.addEventListener("click", () => {
+    addEventBtn.style.display = 'none';
+    eventDesc.forEach(el => { el.style.display = 'grid' });
+})
+
+let timeDifference; // variable used to determine how much the user can write for the description as well as the visible length of the event
+descInput.addEventListener("input", () => {
+    timeDifference = parseInt(chooseTo.value) - parseInt(chooseFrom.value);
+    descInput.maxLength = timeDifference * 19;
+
+    // checks if letter limit is reached and sends error code to inform user
+    if (descInput.value.length === timeDifference * 19) {
+        let infoMessage = "NOTE: max description limit reached. Tip: you can choose a longer lasting event for more typing space.";
+        errorEventCreation.textContent = infoMessage;
+        errorEventCreation.style.display = 'inline-block';
+    }
+})
+
+// coordinates for the new event in the calendar grid.
+let xEvent;
+let yEvent;
+let calendarData = {
+    // groups object for the different groups created by the user
+    groups: {
+        groupNames: ["sport"],
+
+        sport: {
+            color: "orange",
+            events: []
+        }
+    },
+
+    // events object to systematically create event objects that then get added into their adherent group
+    all_events: {
+        num: 0
+
+    }
+};
+
+createEventBtn.addEventListener("click", () => {
+    // CODE TO CHECK IF GIVEN INFO BY USER IS VALID
+    
+    let errorMessage = "Error: ";
+    let error = false;
+    let missingCounter = 0;
+    let eventHeight;
+
+    //checks if the chosen time period is legitimate
+    if (parseInt(chooseFrom.value) >= parseInt(chooseTo.value)) {
+        error = true;
+        errorMessage += "time space chosen not valid, "
+    }
+
+    //checks if a group has been assigned / chosen
+    if (!groupInput.value) {
+        error = true;
+        errorMessage += "missing group"
+        missingCounter += 1;
+    }
+
+    //checks if the description is added
+    if (!descInput.value) {
+        error = true;
+        if (missingCounter === 1) {
+            errorMessage += " and description";
+        } else {
+            errorMessage += "missing description";
+        }
+    }
+
+    //error message gets sent if errors have been found
+    if (error) {
+        errorEventCreation.textContent = errorMessage;
+        errorEventCreation.style.display = 'inline-block';
+    } else {
+        console.log("good");
+        //calculation of event coordinates in the calendar grid
+        xEvent = chooseDay.value * 6 + 1;
+        yEvent = chooseFrom.value;
+        eventHeight = chooseTo.value - chooseFrom.value;
+
+
+        // CHECKS IF THE GROUP ASSIGNED IS NEW
+        let groupIsNew = true;
+        let color = "lightblue";  //this needs to be changed to the color from the database (based on the group of the event)
+        for (const key in calendarData.groups) {
+            if (key === groupInput.value) {
+                groupIsNew = false;
+                color = calendarData.groups[key].color;
+            }
+        }
+
+        // CODE FOR HIDING THE EVENT CREATION AND PUTTING BACK THE "ADD EVENT" BUTTON
+        eventDesc.forEach(el => { el.style.display = 'none' });
+        errorEventCreation.style.display = 'none';
+
+        // CODE FOR THE CREATION OF THE EVENT
+
+        let newEvent = document.createElement('p');
+        newEvent.classList.add("new-event");
+
+        // updates amount of events, and creates the event name (eventx, for x amount of total events)
+        calendarData.all_events.num += 1;
+        const eventName = `event${calendarData.all_events.num}`;
+
+        // if the group assigned is new, the user gets to choose its color
+        if (groupIsNew) {
+            colorChoice.style.display = 'grid';
+
+            // adds the new group to the groupName array that keeps track of all group names ( needed for the for loop to load in the data )
+            calendarData.groups.groupNames.push(groupInput.value);
+            
+            colorOkButton.addEventListener("click", () => {
+                // saves the chosen color, removes the color-choice div and shows the add event button
+                color = colorPicker.value;
+                colorChoice.style.display = 'none';
+                addEventBtn.style.display = 'block';
+
+                // adds the chosen color and adds the new group to the calendar database
+                newEvent.style.backgroundColor = color;
+
+                calendarData.all_events[eventName] = {
+                    date: findDate(),
+                    time: `${chooseFrom.value} --> ${chooseTo.value}`,
+                    desc: descInput.value
+                }
+
+                calendarData.groups[groupInput.value] = {
+                    color: color,
+                    events: [calendarData.all_events.eventName]
+                }
+
+            }, {once: true}); // makes sure that this event listener for the specific element gets deactivated after one use, to avoid color confusion (all elements adapting the new color chosen)
+        } else {
+            addEventBtn.style.display = 'block';
+            newEvent.style.backgroundColor = color;
+            //adds new event data to the already existing group
+            calendarData.all_events[eventName] = {
+                date: findDate(),
+                time: `${chooseFrom.value} --> ${chooseTo.value}`,
+                desc: descInput.value
+            }
+
+            calendarData.groups[groupInput.value].events.push(calendarData.all_events.eventName);
+        }
+
+        newEvent.textContent = descInput.value;
+        newEvent.style.marginLeft = `${xEvent}rem`;
+        newEvent.style.marginTop = `${yEvent}rem`;
+        newEvent.style.height = `${eventHeight}rem`;
+        grid.appendChild(newEvent);
+    }
+
+})
+
+// calendar navigation
+
+
+// load in data
+displayWeek(cDate, cMonthNum, cYear);
+
+for (let i = 0; i < calendarData.groups.groupNames.length; i++) {
+    // assigns all events of the i-th group
+    let groupEvents = calendarData.groups[calendarData.groups.groupNames[i]].events;
+
+    // loop to go through every event in the i-th group
+    for (let y = 0; y < groupEvents.length; y++) {
+        let eventDate = calendarData.all_events.groupEvents[y].date;
+
+    }
+}
+
